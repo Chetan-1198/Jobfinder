0405727bdd4354c420372c43b73eebea74395ccb
Chetan-1198 <chetannikam1198@gmail.com>
2026-01-20 21:53:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -127,3 +127,8 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(debug=True, port=5001)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
+
